cd1911512e29926304e1dae59d26ecb9d79f73f3
Brajendra702 <vijendras452@gmail.com>
2023-03-05 17:55:23 +0530

    Solution

--- src/main/java/com/driver/SpotifyController.java
+++ src/main/java/com/driver/SpotifyController.java
@@ -84,12 +84,14 @@ public class SpotifyController {
     @GetMapping("/popular-artist")
     public String mostPopularArtist(){
         //Return the artist name with maximum likes
+        return spotifyService.mostPopularArtist();
 
     }
 
     @GetMapping("/popular-song")
     public String mostPopularSong(){
         //return the song title with maximum likes
+        return spotifyService.mostPopularSong();
 
     }
 }
